8a26e6f6a3efd35d770fb96ac663b84217d28d90
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-11-18 01:58:14 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -224,14 +224,14 @@ async def youtube_dl_call_back(bot, update):
                     #width = metadata.get("width")
                 #if metadata.has("height"):
                     #height = metadata.get("height")
-                if tg_send_type == "vm":
+                #if tg_send_type == "vm":
                     #height = width
                 # resize image
                 # ref: https://t.me/PyrogramChat/44663
                 # https://stackoverflow.com/a/21669827/4723940
-                Image.open(thumb_image_path).convert(
-                    "RGB").save(thumb_image_path)
-                img = Image.open(thumb_image_path)
+                #Image.open(thumb_image_path).convert(
+                    #"RGB").save(thumb_image_path)
+                #img = Image.open(thumb_image_path)
                 # https://stackoverflow.com/a/37631799/4723940
                 # img.thumbnail((90, 90))
                 if tg_send_type == "file":
